b106b3a825c63d41219a0fe2ef8cd36e1c5a0cd0
Jakub Gocławski <jakub.goclawski@socialwifi.com>
2016-12-29 14:13:06 +0100

    Validate fields used to create models.

--- queue_messaging/data/structures.py
+++ queue_messaging/data/structures.py
@@ -10,6 +10,7 @@ class Model:
         raise NotImplementedError
 
     def __init__(self, **kwargs):
+        self._validate_with_schema_fields(kwargs, self.Meta.schema())
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,3 +19,21 @@ class Model:
             self.__class__.__name__,
             (', '.join(['='.join((key, repr(value))) for key, value in self.__dict__.items()])),
         )
+
+    @staticmethod
+    def _validate_with_schema_fields(model_fields, schema):
+        model_fields_names = set(model_fields.keys())
+        schema_fields_names = set(schema.fields.keys())
+        schema_required_fields_names = {
+            field_name
+            for field_name, field in schema.fields.items()
+            if field.required
+        }
+        unexpected_fields = model_fields_names - schema_fields_names
+        if len(unexpected_fields) > 0:
+            raise TypeError("Got unexpected fields '{}'".format(
+                unexpected_fields))
+        missing_required_fields = schema_required_fields_names - model_fields_names
+        if len(missing_required_fields) > 0:
+            raise TypeError("Missing required fields '{}'".format(
+                missing_required_fields))
---
+++ tests/data/test_structures.py
@@ -0,0 +1,50 @@
+import marshmallow
+import pytest
+from marshmallow import fields
+
+from queue_messaging.data import structures
+
+
+class FancyModelSchema(marshmallow.Schema):
+    uuid_field = fields.UUID(required=True)
+    string_field = fields.String(required=False)
+
+
+class FancyModel(structures.Model):
+    class Meta:
+        schema = FancyModelSchema
+
+
+def test_creating_model():
+    model = FancyModel(
+        uuid_field=1,
+        string_field='123456789',
+    )
+    assert model.uuid_field == 1
+    assert model.string_field == '123456789'
+
+
+def test_if_creating_model_with_missing_optional_field_is_ok():
+    model = FancyModel(
+        uuid_field=1,
+    )
+    assert model.uuid_field == 1
+    assert not hasattr(model, 'string_field')
+
+
+def test_if_creating_model_with_invalid_fields_raises_exception():
+    with pytest.raises(TypeError) as excinfo:
+        FancyModel(
+            uuid_field=1,
+            string_field='123456789',
+            not_existing_field='abc'
+        )
+    assert str(excinfo.value) == "Got unexpected fields '{'not_existing_field'}'"
+
+
+def test_if_creating_model_with_missing_required_field_raises_exception():
+    with pytest.raises(TypeError) as excinfo:
+        FancyModel(
+            string_field='aaa',
+        )
+    assert str(excinfo.value) == "Missing required fields '{'uuid_field'}'"
