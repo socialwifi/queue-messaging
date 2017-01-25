a3446712507965d59360d8f2aed4d2bc176330e2
Jakub Gocławski <jakub@goclawski.net>
2017-01-25 17:58:24 +0100

    Revert support for Python 3.6.
    Travis has problems with building from source and wheels only work for 3.5 right now.

--- setup.py
+++ setup.py
@@ -24,6 +24,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ]
 )
